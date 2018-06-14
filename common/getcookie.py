7c820aa01c05279679a5687ab1590bc91643a02e
wjq0 <1428434957@qq.com>
2018-06-14 20:02:41 +0800

    614

--- common/getcookie.py
+++ common/getcookie.py
@@ -1,7 +1,7 @@
 import requests
 import json
 s=requests.session()
-response=s.post('http://pc4.shein.com/user/auth/login',{'email':'test2@666.com','password':'123456'})
+response=s.post('https://www.shein.com/user/auth/login',{'email':'test@s.com','password':'123456'})
 cookie=response.cookies
 c=requests.cookies.RequestsCookieJar()
 c.set('memberId',cookie['memberId'])
@@ -9,6 +9,6 @@ c.set('userinfo_email',cookie['userinfo_email'])
 c.set('userinfo_userId',cookie['userinfo_userId'])
 s.cookies.update(c)
 
-r=s.get('http://pc4-au.shein.com/user/wishlist/add?goods_id=70')
+r=s.get('http://www.shein.com/user/wishlist/add?goods_id=7090')
 print(r)
 print(r.text)
--- testcase/test_login.py
+++ testcase/test_login.py
@@ -1,16 +1,17 @@
 import common.httpqingqiu
 import common.getexcel
 import unittest
-logincase=common.getexcel.getcase("testcase.xlsx","login")
+logincase=common.getexcel.getcase(r"d:\apitest\testcase.xlsx","login")
 import json
 class Testlogin(unittest.TestCase):
     def setUp(self):
-        self.header={'Content-Type': 'application/json; charset=utf-8'}
+        pass
+        #self.header={'Content-Type': 'application/json; charset=utf-8'}
     def test_loginapi(self):
         for i in range(len(logincase)):
-            response_text=common.httpqingqiu.HttpReq(logincase[i]['url'],json.loads(logincase[i]['casedata']),logincase[i]['casemethod'],self.header).httprequest()
+            response_text=common.httpqingqiu.HttpReq(logincase[i]['url'],json.loads(logincase[i]['data']),logincase[i]['method']).httprequest()
             res=json.loads(response_text)
-            self.assertEqual(res["msg"],logincase[i]['caseqiwang'])
+            self.assertEqual(res["msg"],logincase[i]['qiwang'])
     def tearDown(self):
         pass
 if __name__=="__main__":
